cfc1fcb422c7f45943ba03ca922d3f50f39fd056
Jordann-Araujo <79881822+Jordann-Araujo@users.noreply.github.com>
2021-12-28 14:33:04 -0300

    Inserção 04 - Insert de usuario.js

---
+++ dados/usuario_.js
@@ -0,0 +1,27 @@
+export default [
+
+  {
+    id: 1,
+    nome: "José Lucas de Araújo",
+    descricao: "José Lucas é o CEO de uma empresa  especializada  em equipamentos de segurança residencial inovador e de última geração, Security Esp.",
+    conta_usuario: 'lukinha.078',
+    conta_senha: "1234",
+    cont_email: 'hehe@email.com'
+  },
+  {
+    id: 2,
+    nome: "Jordann Cyrino Araújo",
+    conta_usuario: 'jordann_123',
+    descricao: '',
+    conta_senha: "1234",
+    cont_email: 'opa@email.com'
+  },
+  {
+    id: 3,
+    nome: "Kethilly Nayara Felix",
+    descricao: '',
+    conta_usuario: 'ket.felix',
+    conta_senha: "1234",
+    cont_email: 'ket@email.com'
+  }
+]
